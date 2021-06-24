273f1dc1298d97d56c8e3a5365341469e5770c11
Luiz Carlos Martins <lc84martins@gmail.com>
2021-06-24 11:50:43 -0300

    upando appconfig

--- src/main/java/br/org/serratec/backend/config/AppConfig.java
+++ src/main/java/br/org/serratec/backend/config/AppConfig.java
@@ -4,7 +4,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class AppConfig implements WebMvcConfigurer {
-	
+	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
